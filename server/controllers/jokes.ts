14c1eebd6f1804c2dd4f123276b73e5f044edf00
Glen Jarvis <glen@glenjarvis.com>
2025-09-23 12:28:23 -0700

    iterate

--- server/controllers/jokes.ts
+++ server/controllers/jokes.ts
@@ -16,7 +16,30 @@ export async function dbSaveJoke(
   response: Response,
   next: NextFunction
 ) {
-  console.log(`joke: ${JSON.stringify(request.body)}`);
+  if (!request.body) {
+    next({
+      log: `request.body is empty (no joke)`,
+      message: { err: 'Joke not included in body' },
+    });
+  }
+
+  if (!request.body.hasOwnProperty('joke')) {
+    next({
+      log: `"joke" key not found in: request.body`,
+      message: { err: 'body does not have "joke" key' },
+    });
+  }
+
+  const joke = request.body.joke;
+
+  if (!joke.text || !joke.source) {
+    next({
+      log: `"joke" object does not have "text" and "source": ${JSON.stringify(joke)}`,
+      message: {
+        err: 'Invalid joke contents: "text" and "source" are required.',
+      },
+    });
+  }
   // Save to database
   response.locals.dbResponse = 'Joke (not really) saved to database.';
   next();
